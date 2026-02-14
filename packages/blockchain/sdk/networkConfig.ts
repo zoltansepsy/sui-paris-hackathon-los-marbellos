8c59d363b23a66a8dee6d65d708d34a466059510
Zoltan Sepsy <zoltan.sepsy.external@zalando.de>
2026-02-14 08:34:39 +0100

    update package id

--- packages/blockchain/sdk/networkConfig.ts
+++ packages/blockchain/sdk/networkConfig.ts
@@ -12,7 +12,12 @@ const { networkConfig, useNetworkVariable, useNetworkVariables } =
     testnet: {
       url: getFullnodeUrl("testnet"),
       variables: {
-        // Add package IDs after deployment, e.g. packageId: "0x...",
+        packageId:
+          "0x470bfc45bf7dbc92bed9bf723ca7335b189332a909f5d1370622993600876666",
+        platformId:
+          "0xb9010ffc6672232da2a699d092bc6cd7ebf2afba02588527d5b608f7690cdb2c",
+        registryId:
+          "0x8effd5679d0bbd647b31cb7843b269b4f16c7714fa51712a043bc058a5aac5d8",
       },
     },
     mainnet: {
